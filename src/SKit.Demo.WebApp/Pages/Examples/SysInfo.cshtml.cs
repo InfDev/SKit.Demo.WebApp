b1e045f90ad7d2032c1a859080df0ab80e7fe2a2
InfDev <alex.shlyakhto@gmail.com>
2020-05-17 17:34:26 +0300

    Added SysInfo page

---
+++ src/SKit.Demo.WebApp/Pages/Examples/SysInfo.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using SKit.Common.Models;
+using SKit.Common.Helpers;
+
+namespace SKit.Demo.WebApp.Pages.Examples
+{
+    public class SysInfoModel : PageModel
+    {
+        public IEnumerable<ValueText> Infos { get; set; }
+        
+        public void OnGet()
+        {
+            Infos = SystemHelper.GetSystemInfo();
+        }
+    }
+}
